79f0537da424c3165d791f71009e672b9b73732c
Justin Hellen <jmhellen1@gmail.com>
2019-09-21 14:15:23 -0400

    added initial.js

---
+++ initial.js
@@ -0,0 +1,42 @@
+let store = {
+    questionNumberOne: {
+    text: "Who was the original (and best, by the way) bass guitar player in the band Metallica?",
+    answer: "Cliff Burton"
+  },
+    questionNumberTwo: {
+    text: "What Roots Rock artist was lead singer for the band The Wailers?",
+    answer: "Bob Marley"
+  },
+    questionNumberThree: {
+    text: "What band was formed out of the remaining members of the failed Black Metal band Hellhammer?",
+    answer: "Celtic Frost"
+  },
+    questionNumberFour: {
+    text: "What famous vocalist was fired from Black Sabbath in 1979 before starting his solo career?",
+    answer: "Ozzy Osbourne"
+  },
+    questionNumberFive: {
+    text: "What band is largely credited for pioneering the Death Metal genre?",
+    answer: "Death"
+  },
+    questionNumberSix: {
+    text: "What is the most widely recognized nickname of the late lead guitarist Darrell Abbot of the band Pantera?",
+    answer: "Dimebag"
+  },
+    questionNumberSeven: {
+    text: "With what band did famous singer Iggy Pop start his music career?",
+    answer: "The Stooges"
+  },
+    questionNumberEight: {
+    text: "What prominent lead singer/lead guitarist duo came from the band Led Zeppelin?",
+    answer: "Robert Plant and Jimmy Paige"
+  },
+    questionNumberNine: {
+    text: "What vocalist pioneered his career in Heavy Metal after multiple attempts at leading Punk Rock bands such as The Misfits and Samhain?",
+    answer: "Glenn Danzig"
+  },
+    questionNumberTen: {
+    text: "What was the name of Jimi Hendrix' first band (hint: circa 1965)?",
+    answer: "Jimmy James and the Blue Flames"
+  }
+};
