a7db36c83a60661eb2a762a49ea016c940758e42
Justin Hellen <jmhellen1@gmail.com>
2019-09-23 11:48:34 -0400

    minor change to answer feedback

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ submitButton.click(function(e) {
     buttonSwitchNext();
     if (inputUserValue === correctAnswerPointer) {
       greenAnswerTag.removeClass('invisible');
-      $('#multipleChoice').replaceWith(`<div id="multipleChoice">Correct answer: ${correctAnswerPointer}</div>`);
+      $('#multipleChoice').replaceWith(`<div id="multipleChoice">Answer: ${correctAnswerPointer}</div>`);
       score++;
       $('#score').replaceWith(`
         <div class="score" id="score">
@@ -125,7 +125,7 @@ submitButton.click(function(e) {
       `);
     } else {
       redAnswerTag.removeClass('invisible');
-      $('#multipleChoice').replaceWith(`<div id="multipleChoice">Correct answer: ${correctAnswerPointer}</div>`);
+      $('#multipleChoice').replaceWith(`<div id="multipleChoice">Answer: ${correctAnswerPointer}</div>`);
     }
   } else {
     alert('Choose an answer before you click submit, Wastoid!');
